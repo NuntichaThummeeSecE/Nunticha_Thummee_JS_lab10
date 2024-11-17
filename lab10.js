dd5dfb9fc649943d3f7e260fa498e3ade3482732
NuntichaThummeeSecE <n01687478@humber.ca>
2024-11-16 23:03:43 -0500

    task1

---
+++ lab10.js
@@ -0,0 +1,13 @@
+//Task 1: What is API?
+/* API: Application Programming Interface 
+It is a middleware that allows two systems or applications to communicate through an agreed-upon protocol.
+When one app needs data or services from another app, it sends a request to the server via API.
+The server will respond with the requested information.
+
+Example 
+Google Map API: You open the restaurant. 
+When a customer orders for delivery, you need to find their exact location.
+You can request the address of your customer location sent to Google Maps via the Google Map API.
+Google Maps will return the exact location of the customer's address.
+This allows you to deliver the food to the right place.
+*/
